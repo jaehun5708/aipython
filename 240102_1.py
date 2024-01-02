7b252e1630de09a901c83586e3635b1431a64e4c
jaehun5708 <154937642+jaehun5708@users.noreply.github.com>
2024-01-02 11:51:44 +0900

    Add files via upload

---
+++ 240102_1.py
@@ -0,0 +1,34 @@
+# number = 10
+# result = number*2
+# print(result)
+
+# a=int(input("당신의 나이는?"))
+# print(a, "살 이군요")
+# print("내년엔", a+1, "살이 되네요")
+# print(type(a))
+
+#a=input("당신의 나이는?")
+#msg=a+"살 이군요"
+#print(msg)
+#print("내년엔", int(a)+1, "살이 되네요")
+#print(type(a))
+
+#a="파이썬"
+#print(a,"아 반가워")
+#msg=a+"아 반가워"
+#print(msg)
+#print("%s아 반가워~"%a)
+
+# a=24; b=21; c=(a+b)/2
+# print(c)
+# print("%d"%c)
+# print("%4d"%c)
+# print("%f"%c)
+# print("%3.1f"%c)
+# print("%6.2f"%c)
+
+#print("오늘은 {}월 {}일 입니다.".format(1,2))
+
+name="홍길동"
+age=20.5
+print("이름은{:>10s}이고 나이는{:.2f}입니다.".format(name,age))
